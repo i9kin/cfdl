16ff6b318cd4d64a20bb3612bd6b8fe77f2c93b8
9kin <gate.web.tester@mail.ru>
2020-06-30 21:36:40 +0500

    black

--- codeforces2html/__init__.py
+++ codeforces2html/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for codeforces2html."""
 
 __author__ = """Ivan"""
-__email__ = 'cf2html.github@mail.ru'
-__version__ = '0.1.0'
+__email__ = "cf2html.github@mail.ru"
+__version__ = "0.1.0"
--- codeforces2html/cli.py
+++ codeforces2html/cli.py
@@ -6,8 +6,10 @@ import click
 @click.command()
 def main(args=None):
     """Console script for codeforces2html."""
-    click.echo("Replace this message by putting your code into "
-               "codeforces2html.cli.main")
+    click.echo(
+        "Replace this message by putting your code into "
+        "codeforces2html.cli.main"
+    )
     click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
 
