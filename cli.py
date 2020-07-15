39fec4f48448b63218c2e32289bc440a448a2b45
9kin <gate.web.tester@mail.ru>
2020-07-15 13:13:32 +0500

    pdf

--- cli.py
+++ cli.py
@@ -23,7 +23,8 @@
 # 2. --with-solution --with-code pdf command
 # 3. code for task from top
 # 4. link parser
-
+# COMAND html + pdf
+# --div(1/2/3) --range='A'-'B'
 import io
 
 from rich.traceback import install
@@ -39,20 +40,40 @@ from codeforces2html import xhr
 from codeforces2html.models import clean_database
 from codeforces2html.utils import clean_contests, clean_tasks, problemset
 
+
+header = io.StringIO()
+header_console = Console(file=header, force_terminal=True)
+header_console.print(
+    f":bar_chart:[white bold]codeforces {__version__}[/]\n"
+    + f"Usage: cli.py download [red bold][[OPTIONS]][/] [magenta bold][[ARGUMENTS]]...[/]"
+)
+
 download_options = io.StringIO()
-console = Console(file=download_options, force_terminal=True)
-console.print(
-    "[red bold]Options:[/]\n"
-    + "  [cyan bold]--clean[/]        [white bold]Clean database before start[/white bold]\n"
-    + "  [cyan bold]-t --tutorial[/]  [white bold]Save tutorial.[/white bold]\n"
+download_console = Console(file=download_options, force_terminal=True)
+download_console.print(
+    "[red bold]Options for download command:[/]\n"
+    + "  [cyan bold]--clean[/]        [white bold]Clean database before download.[/white bold]\n"
+    + "  [cyan bold]-t --tutorial[/]  [white bold]Download tutorial.[/white bold]\n"
     + "  [cyan bold]--pdf[/]          [white bold]Generate pdf after download.[/white bold]\n"
     + "  [cyan bold]--debug[/]        [white bold]Show progress bar.[/white bold]\n"
-    + "  [cyan bold]--help[/]         [white bold]Show this message and exit.[/white bold]\n"
-    + "[magenta bold]Arguments:[/]\n"
+)
+
+arguments = io.StringIO()
+arguments_console = Console(file=arguments, force_terminal=True)
+arguments_console.print(
+    "[magenta bold]Arguments:[/]\n"
     + "  [yellow bold]conetst[/]        {id}          Like 1364\n"
     + "  [yellow bold]conetst-range[/]  {start}-{end} Like 1364-1365\n"
     + "  [yellow bold]task[/]           {id}{letter}  Like [blue bold]1364A[/]\n"
 )
+
+pdf_options = io.StringIO()
+pdf_console = Console(file=pdf_options, force_terminal=True)
+pdf_console.print(
+    "[red bold]Options for pdf command:[/]\n"
+    + "  [cyan bold]--debug[/]        [white bold]Show progress bar.[/white bold]\n"
+)
+
 
 
 class OrderedGroup(click.Group):
@@ -60,54 +81,42 @@ class OrderedGroup(click.Group):
         super().__init__(name, **attrs)
 
     def format_help(self, ctx, formatter):
-        sio = io.StringIO()
-        console = Console(file=sio, force_terminal=True)
-        console.print(
-            f":bar_chart:[white bold]codeforces {__version__}[/white bold]"
-        )
-
-        formatter.write(sio.getvalue())
+        formatter.write(header.getvalue())
         self.format_commands(ctx, formatter)
 
     def format_commands(self, ctx, formatter):
-        formatter.write("Usage for download\n")
         formatter.write(download_options.getvalue())
-
+        formatter.write(pdf_options.getvalue())
+        formatter.write(arguments.getvalue())
 
 class CommandHelp(click.Command):
-    def __init__(self, name=None, **attrs):
+    def __init__(self, name=None, option=None, **attrs):
+        self.option = option
         super().__init__(name, **attrs)
 
     def format_help(self, ctx, formatter):
-        sio = io.StringIO()
-        console = Console(file=sio, force_terminal=True)
-        console.print(
-            f":bar_chart:[white bold]codeforces {__version__}[/]\n"
-            + f"Usage: t.py download [red bold][[OPTIONS]][/] [magenta bold][[ARGUMENTS]]...[/]"
-        )
-        formatter.write(sio.getvalue())
+        formatter.write(header.getvalue())
         self.format_options(ctx, formatter)
+        formatter.write(arguments.getvalue())
 
     def format_options(self, ctx, formatter):
-        # super().format_options(ctx, formatter)
-        formatter.write(download_options.getvalue())
+        formatter.write(self.option.getvalue())
 
+
+class DownloadHelp(CommandHelp):
+    def __init__(self, name=None, **attrs):
+        super().__init__(name, **attrs, option=download_options)
+
+class PdfHelp(CommandHelp):
+    def __init__(self, name=None, **attrs):
+        super().__init__(name, **attrs, option=pdf_options)
 
 @click.group(cls=OrderedGroup)
 def cli():
     pass
 
 
-@cli.command(cls=CommandHelp)
-@click.option("--pdf", is_flag=True)
-@click.option("--debug", default=True, is_flag=True)
-@click.option("--clean", default=False, is_flag=True)
-@click.option("-t", "--tutorial", default=False, is_flag=True)
-@click.argument("arguments", nargs=-1)
-def download(arguments, clean, tutorial, pdf, debug):
-    RCPC = "00d28833206ccd9a67176c3190299d6e"
-    if clean:
-        clean_database()
+def parse_arguments(arguments):
     download_contests = set()
     download_tasks = set()
 
@@ -126,6 +135,20 @@ def download(arguments, clean, tutorial, pdf, debug):
             download_contests.add(int(argument))
     download_contests = clean_contests(download_contests)
     download_tasks = clean_tasks(list(download_tasks))
+    return download_contests, download_tasks
+
+@cli.command(cls=DownloadHelp)
+@click.option("--pdf", is_flag=True)
+@click.option("--debug", default=True, is_flag=True)
+@click.option("--clean", default=False, is_flag=True)
+@click.option("-t", "--tutorial", default=False, is_flag=True)
+@click.argument("arguments", nargs=-1)
+def download(arguments, clean, tutorial, pdf, debug):
+    RCPC = "00d28833206ccd9a67176c3190299d6e"
+    download_contests, download_tasks = parse_arguments(arguments)
+    
+    if clean:
+        clean_database()
 
     codeforces2html.main(download_contests, download_tasks, RCPC, debug=debug)
 
@@ -133,6 +156,17 @@ def download(arguments, clean, tutorial, pdf, debug):
         xhr.main(download_contests, download_tasks, RCPC, debug=debug)
     if pdf:
         topdf.pdf(download_contests, download_tasks)
+
+
+@cli.command(cls=PdfHelp)
+@click.option("--debug", default=True, is_flag=True)
+@click.argument("arguments", nargs=-1)
+def pdf(arguments, debug):
+    download_contests, download_tasks = parse_arguments(arguments)
+    print(download_contests, download_tasks)
+    #print(arguments, debug)
+    #if pdf:
+    topdf.html(download_contests, download_tasks, ['A', 'B', 'C'])
 
 
 if __name__ == "__main__":
--- codeforces2html/pdf.py
+++ codeforces2html/pdf.py
@@ -37,5 +37,32 @@ def pdf(contests, additional_tasks):
     html = render_tasks(tasks, solutions_array)
     pdfkit.from_string(html, "out.pdf", options=options)
 
+def html(contests, additional_tasks, tasks_range):
+    all_tasks = additional_tasks.copy() + get_tasks(contests)
+    all_tasks = [str(contest_id) + letter for contest_id, letter in all_tasks]
+
+    solutions_array = SolutionsArray(Solutions.select().dicts())
+    tasks = Tasks.select().where(Tasks.id << [task for task in all_tasks])
+    tasks = [task for task in tasks]
+
+    new = []
+    for task in tasks:
+        if 'Див. 2' in task.contest_title or 'Div. 2' in task.contest_title:
+            if len(task.tutorial) != 0:
+                for i, char in enumerate(task.id):
+                    if not char.isdigit():
+                        id = task.id[i:]
+                        break
+
+                if id in tasks_range:
+                    new.append(task)
+
+    options = {
+        "page-size": "Letter",
+        "no-outline": None,
+        "javascript-delay": 2 * len(new) * 1000,
+    }
+    html = render_tasks(new, solutions_array)
+    pdfkit.from_string(html, "out.pdf", options=options)
 
 __all__ = ["dir_path", "env", "pdf", "render_tasks"]
